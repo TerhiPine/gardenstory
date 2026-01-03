25b99f225f5c2a7a1343d85e4f917e0083232fd1
TerhiPine <terhi.petajajarvi@gmail.com>
2026-01-03 16:45:51 +0200

    säädetty splashscreeniä

--- frontend/gardenstory/src/App.jsx
+++ frontend/gardenstory/src/App.jsx
@@ -1,25 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SplashScreen from "./components/SplashScreen";
 import Home from "./pages/Home";
 
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
+export default function App() {
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000); // 3 sekuntia
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isLoading) {
-    return <SplashScreen />;
-  }
-
-  return <Home />;
+  return (
+    <>
+      {loading ? (
+        <SplashScreen onFinish={() => setLoading(false)} />
+      ) : (
+        <Home />
+      )}
+    </>
+  );
 }
 
-export default App;
 
 
--- frontend/gardenstory/src/components/SplashScreen.jsx
+++ frontend/gardenstory/src/components/SplashScreen.jsx
@@ -6,9 +6,10 @@ const messages = [
   "Multa tuoksuu raikkaalta...",
 ];
 
-export default function SplashScreen() {
+export default function SplashScreen({ onFinish }) {
   const [index, setIndex] = useState(0);
 
+  // Tekstin vaihtaminen
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % messages.length);
@@ -17,13 +18,22 @@ export default function SplashScreen() {
     return () => clearInterval(interval);
   }, []);
 
+  // SplashScreenin kesto 3 sekuntia
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onFinish(); // vanhemmalle komponentille ilmoitetaan, että splash on valmis
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [onFinish]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen bg-green-100">
       <h1 className="text-4xl font-bold text-green-900 mb-10">
         Garden Story
       </h1>
 
-      <p className="absolute bottom-8 text-green-700 animate-pulse">
+      <p className="absolute bottom-8 sm:bottom-4 text-green-700 animate-pulse">
         {messages[index]}
       </p>
     </div>
